81b914df48820ee13ce3bd251f245d06fc7341af
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-12-05 07:48:38 +0100

    Add categories to the list of products to seperate the drinks from snacks

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
 const products = [
-    { id: 1, name: 'Celsius', img: './Images/Celsius.png', price: 20.00 },
-    { id: 2, name: 'Arboga 10.2 % Vol', img: './Images/Arboga.png', price: 2.99 },
-    { id: 3, name: 'Coca Cola', img: './Images/CocaCola.png', price: 10.00 },
-    { id: 4, name: 'Coca Cola Zero', img: './Images/ColaZero.png', price: 10.00 },
-    { id: 5, name: 'Dr Pepper', img: './Images/DrPepper.png', price: 10.00 },
-    { id: 6, name: 'Fanta', img: './Images/Fanta.png', price: 10.00 },
-    { id: 7, name: "Jack Daniel's Coca Cola", img: './Images/Jack.png', price: 75.00 },
-    { id: 8, name: 'Red Bull', img: './Images/RedBull.png', price: 25.00 },
-    { id: 9, name: 'Zeunerts Julmust', img: './Images/ZeunertsJulmust.png', price: 30.00 },
-    { id: 10, name: 'Zeunerts Julmust Sockerfri', img: './Images/ZeunertsZero.png', price: 30.00 },
-    { id: 11, name: 'Cheetos Cheddar Jalapeño', img: './Images/Cheetos.png', price: 40.00 },
-    { id: 12, name: 'Dumle Snacks Mint', img: './Images/Dumle.png', price: 35.00 },
-    { id: 13, name: 'Kina Salty Snacks', img: './Images/Kina2.png', price: 39.00 },
-    { id: 14, name: 'Kina Snacks', img: './Images/Kina.png', price: 39.00 },
-    { id: 15, name: 'Ahlgrens Bilar', img: './Images/Ahlgrens.png', price: 21.90 },
-    { id: 16, name: 'Sweet Chilimayo Chips', img: './Images/Olw.png', price: 40.00 },
-    { id: 17, name: 'gooh! Köttbullar', img: './Images/Köttbullar.png', price: 55.90 },
-    { id: 18, name: 'gooh! Lasagnette', img: './Images/Lasagnette.png', price: 55.90 },
-    { id: 19, name: 'Wasa French Herbs', img: './Images/WasaFrenchHerbs.png', price: 15.00 },
-    { id: 20, name: 'Wasa Sourcream & Onion', img: './Images/WasaSourCream.png', price: 15.00 },
-    { id: 21, name: 'Wasa Tomato Basil', img: './Images/WasaTomatoBasil.png', price: 15.00 },
-    { id: 22, name: 'Åsna', img: './Images/Donkey.png', price: 8599.00 }
+    { id: 1, name: 'Celsius', img: './Images/Celsius.png', price: 20.00, category: 'drinks' },
+    { id: 2, name: 'Arboga 10.2 % Vol', img: './Images/Arboga.png', price: 2.99, category: 'drinks' },
+    { id: 3, name: 'Coca Cola', img: './Images/CocaCola.png', price: 10.00, category: 'drinks' },
+    { id: 4, name: 'Coca Cola Zero', img: './Images/ColaZero.png', price: 10.00, category: 'drinks' },
+    { id: 5, name: 'Dr Pepper', img: './Images/DrPepper.png', price: 10.00, category: 'drinks' },
+    { id: 6, name: 'Fanta', img: './Images/Fanta.png', price: 10.00, category: 'drinks' },
+    { id: 7, name: "Jack Daniel's Coca Cola", img: './Images/Jack.png', price: 75.00, category: 'drinks' },
+    { id: 8, name: 'Red Bull', img: './Images/RedBull.png', price: 25.00, category: 'drinks' },
+    { id: 9, name: 'Zeunerts Julmust', img: './Images/ZeunertsJulmust.png', price: 30.00, category: 'drinks' },
+    { id: 10, name: 'Zeunerts Julmust Sockerfri', img: './Images/ZeunertsZero.png', price: 30.00, category: 'drinks' },
+    { id: 11, name: 'Cheetos Cheddar Jalapeño', img: './Images/Cheetos.png', price: 40.00, category: 'snacks' },
+    { id: 12, name: 'Dumle Snacks Mint', img: './Images/Dumle.png', price: 35.00, category: 'snacks' },
+    { id: 13, name: 'Kina Salty Snacks', img: './Images/Kina2.png', price: 39.00, category: 'snacks' },
+    { id: 14, name: 'Kina Snacks', img: './Images/Kina.png', price: 39.00, category: 'snacks' },
+    { id: 15, name: 'Ahlgrens Bilar', img: './Images/Ahlgrens.png', price: 21.90, category: 'snacks' },
+    { id: 16, name: 'Sweet Chilimayo Chips', img: './Images/Olw.png', price: 40.00, category: 'snacks' },
+    { id: 17, name: 'gooh! Köttbullar', img: './Images/Köttbullar.png', price: 55.90, category: 'snacks' },
+    { id: 18, name: 'gooh! Lasagnette', img: './Images/Lasagnette.png', price: 55.90, category: 'snacks' },
+    { id: 19, name: 'Wasa French Herbs', img: './Images/WasaFrenchHerbs.png', price: 15.00, category: 'snacks' },
+    { id: 20, name: 'Wasa Sourcream & Onion', img: './Images/WasaSourCream.png', price: 15.00, category: 'snacks' },
+    { id: 21, name: 'Wasa Tomato Basil', img: './Images/WasaTomatoBasil.png', price: 15.00, category: 'snacks' },
+    { id: 22, name: 'Åsna', img: './Images/Donkey.png', price: 8599.00, category: 'snacks' }
 ];
 
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+
 
 function addToCart(productID) {
     const product = products.find(p => p.id === productID);
@@ -137,3 +139,4 @@ function showSnacks() {
     document.getElementById("snacks").classList.add("active-button");
     document.getElementById("drinks").classList.remove("active-button");
 }
+
